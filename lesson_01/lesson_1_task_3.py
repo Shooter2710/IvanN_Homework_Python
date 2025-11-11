19247ac8e5c4c355e59c2982517511a103184f9c
Shooter2710 <shooter271090@gmail.com>
2025-11-11 23:37:26 +0300

    lesson_01

---
+++ lesson_01/lesson_1_task_3.py
@@ -0,0 +1,4 @@
+first_name = input("Укажите имя: ")
+last_name = input("Укажите фамилию: ")
+print("Вас зовут: " + last_name, end=" ")
+print(first_name)
---
+++ test/test.py
@@ -0,0 +1,23 @@
+my_height=178
+#print(my_height)
+
+my_name="Ivan"
+my_name="Ivan Naryadnov"
+#print(my_name)
+
+#pet_name=input("Как зовут вашего питомца? ")
+#print("Ваш любимчик - "+pet_name)
+
+def print_python():
+    print("Учу Python!")
+print_python()
+
+def print_letter(let):
+    print(let, end="")
+print_letter('С')
+print_letter('т')
+print_letter('у')
+print_letter('д')
+print_letter('е')
+print_letter('н')
+print_letter('т')
