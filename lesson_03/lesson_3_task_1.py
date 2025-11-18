3c8702308b9fd3f140fda8ae9a7deb27d57a00e0
Shooter2710 <shooter271090@gmail.com>
2025-11-18 23:14:31 +0300

    Delete lesson_03 directory

--- lesson_03/lesson_3_task_1.py
+++
@@ -1,7 +0,0 @@
-from user import User
-
-my_user = User("Иван", "Наряднов")
-
-my_user.print_first_name()
-my_user.print_last_name()
-my_user.print_all()
--- lesson_03/lesson_3_task_3.py
+++
@@ -1,9 +0,0 @@
-from address import Address
-from mailing import Mailing
-
-to_address = Address(400066, "Волгоград", "Ленина", 5, 25)
-from_address = Address(350073, "Краснодар", "Командорская", 3, 279)
-
-mailing = Mailing(from_address, to_address, 100, "4564645646")
-
-print(mailing)
