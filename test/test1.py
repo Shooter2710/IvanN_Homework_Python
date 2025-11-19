f365d3e42c78aa7306909420fb52d6d1ff70210a
Shooter2710 <shooter271090@gmail.com>
2025-11-19 15:52:44 +0300

    les4

--- test/test1.py
+++ test/test1.py
@@ -1,10 +1,23 @@
 import pytest
 from string_processor import StringProcessor
 
-stringprocessor = StringProcessor()
-
-@pytest.mark.parametrize('text, result', [("погода", "Погода."), ("хорошая погода", "Хорошая погода."), ("Хорошая погода.", "Хорошая погода.")])
+@pytest.mark.parametrize(
+        'text, result', 
+        [("погода", "Погода."), 
+         ("хорошая погода", "Хорошая погода."), 
+         ("Хорошая погода.", "Хорошая погода.")])
 def positive_tests(text, result):
     stringprocessor = StringProcessor()
-    res = stringprocessor.process(text)
-    assert res == result
+    stringprocessor.process(text) == result
+
+@pytest.mark.parametrize(
+        'text, result', 
+        [("", "."), ("     ", ".")])
+def negative_tests(text, result):
+    stringprocessor = StringProcessor()
+    stringprocessor.process(text) == result
+
+def negative_test():
+    stringprocessor = StringProcessor()
+    with pytest.raises(TypeError):
+        stringprocessor.process(123)
