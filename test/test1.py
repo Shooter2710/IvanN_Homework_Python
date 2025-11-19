3e53885c42f6bf845c060dde1c9972f1d54c3c31
Shooter2710 <shooter271090@gmail.com>
2025-11-19 15:24:16 +0300

    les4

--- test/test1.py
+++ test/test1.py
@@ -4,12 +4,7 @@ from string_processor import StringProcessor
 stringprocessor = StringProcessor()
 
 @pytest.mark.parametrize('text, result', [("погода", "Погода."), ("хорошая погода", "Хорошая погода."), ("Хорошая погода.", "Хорошая погода.")])
-def positive_tests(text: str, result: str):
+def positive_tests(text, result):
     stringprocessor = StringProcessor()
     res = stringprocessor.process(text)
     assert res == result
-
-# def positive_test():
-#     stringprocessor = StringProcessor()
-#     res = stringprocessor.process("погода")
-#     assert res == "Погода."
