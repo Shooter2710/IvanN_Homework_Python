468f47c14c12cd834064fa16b4777329f228d6f9
Shooter2710 <shooter271090@gmail.com>
2025-11-27 19:55:46 +0300

    lesson_06

---
+++ lesson_06/rename.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+driver.get("http://uitestingplayground.com/textinput")
+
+driver.find_element(By.ID, "newButtonName").send_keys("SkyPro")
+
+driver.find_element(By.ID, "updatingButton").click()
+
+renameButton = driver.find_element(By.ID, "updatingButton").text
+
+print(renameButton)
+
+driver.quit()
--- test/python.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-driver=webdriver.Chrome()
-from selenium.webdriver.common.by import By
-
-driver.get("https://python.org")
-
-driver.find_element(By.CLASS_NAME, "donate-button").click()
-
